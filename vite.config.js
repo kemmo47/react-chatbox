207204db6d7a9eae09500c1be731b49b2ec50d3b
kemmo47 <longkem123456@gmail.com>
2025-09-24 11:20:54 +0700

    feat: add server configuration for CORS and LAN access in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: true, // allow CORS
+    port: 8800, // 👈 choose the port you want
+    host: "0.0.0.0", // 👈 optional: allows access from LAN / other devices
+  },
 });
